fa000be17a14464c806ee5290a43a5936e85d50d
JackeyLea <1768478912@qq.com>
2020-12-27 01:50:11 +0800

    添加模拟数据模块

--- src/generator.cpp
+++ src/generator.cpp
@@ -19,6 +19,118 @@ int Generator::getNum()
     return QRandomGenerator::global()->bounded(0,256);
 }
 
+QByteArray Generator::getRaw()
+{
+    //qDebug()<<"Generating raw data";
+    QByteArray data;
+    data.append(0xAA); //0
+    data.append(0xAA); //1
+    data.append(0x04); //payload
+    data.append(0x80);
+    data.append(0x02);
+    uchar a=getNum();
+    data.append(a);
+    uchar b=getNum();
+    data.append(b);
+    //short rawValue=((uchar)b<<8)|(uchar)a;
+    //qDebug()<<"generated a is: "<<a;
+    //qDebug()<<"generated b is: "<<b;
+    //qDebug()<<"generated rawvalue is: "<<rawValue;
+    int size=data.size();
+    int checksum=0;
+    for(int j=3;j<size;j++){
+        checksum+=data[j];
+    }
+    checksum &= 0xff;
+    checksum = ~checksum & 0xff;
+    data.append(checksum);
+
+//    QVector<uchar> c;
+//    for(int i=0;i<data.size();i++){
+//        c.append((uchar)data[i]);
+//    }
+    //qDebug()<<"data is: "<<c;
+    //qDebug()<<"raw data generated";
+
+    return data;
+}
+
+QByteArray Generator::getEEG()
+{
+    //qDebug()<<"generating eeg data";
+    QByteArray data;
+    data.append(0xAA); //0
+    data.append(0xAA); //1
+    data.append(0x20); //payload
+    //signal
+    data.append(0x02); //signal
+    data.append(getNum()); //signal value
+    //    //power
+    //    data.append(0x01);
+    //    data.append(getNum(128)); //3v
+    //eeg power
+    data.append(0x83); //eeg power
+    data.append(0x18); //eeg power value
+    //delta
+    data.append(getNum());
+    data.append(getNum());
+    data.append(getNum());
+    //theta
+    data.append(getNum());
+    data.append(getNum());
+    data.append(getNum());
+    //lowalpha
+    data.append(getNum());
+    data.append(getNum());
+    data.append(getNum());
+    //highalpha
+    data.append(getNum());
+    data.append(getNum());
+    data.append(getNum());
+    //lowbeta
+    data.append(getNum());
+    data.append(getNum());
+    data.append(getNum());
+    //highbeta
+    data.append(getNum());
+    data.append(getNum());
+    data.append(getNum());
+    //lowgamma
+    data.append(getNum());
+    data.append(getNum());
+    data.append(getNum());
+    //middlegamma
+    data.append(getNum());
+    data.append(getNum());
+    data.append(getNum());
+    //attention
+    data.append(0x04);
+    int t=getNum(101);
+    data.append(t);
+    //meditation
+    data.append(0x05);
+    data.append(getNum(101));
+
+    //calculate sumcheck
+    int checksum = data[3];
+    for (int i = 4; i < data.size(); i++)
+    {
+        checksum += data[i];
+    }
+    checksum &= 0xff;
+    checksum = ~checksum & 0xff;
+    data.append(checksum);
+
+//    QVector<uint> a;
+//    for(int i=0;i<data.size();i++){
+//        a.append((uint)data[i]);
+//    }
+//    qDebug()<<a;
+
+    //qDebug()<<"eeg data generated";
+    return data;
+}
+
 void Generator::stopNow()
 {
     isStop=true;
@@ -35,84 +147,11 @@ void Generator::run()
         //输出time s的模拟数据
         for (int i = 0; i < 511; i++)
         {//每秒钟约512个包
-            QByteArray data;
-            data.append(0xAA); //0
-            data.append(0xAA); //1
-            data.append(0x04);    //payload
-            data.append(0x80);
-            data.append(0x02);
-            data.append(getNum());
-            data.append(getNum());
-            int size=data.size();
-            int checksum=data[size-1]+data[size-2];
-            checksum &= 0xff;
-            checksum = ~checksum & 0xff;
-            data.append(checksum);
-            emit sendData(data);
+            QByteArray ba = getRaw();
+            emit sendData(ba);
         }
-        //qDebug()<<eegPkg();
-        QByteArray data;
-        data.append(0xAA); //0
-        data.append(0xAA); //1
-        data.append(0x20);    //payload
-        //signal
-        data.append(0x02);        //signal
-        data.append(getNum(256)); //signal value
-    //    //power
-    //    data.append(0x01);
-    //    data.append(getNum(128)); //3v
-        //eeg power
-        data.append(0x83); //eeg power
-        data.append(0x18);    //eeg power value
-        //delta
-        data.append(getNum(256));
-        data.append(getNum(256));
-        data.append(getNum(256));
-        //theta
-        data.append(getNum(256));
-        data.append(getNum(256));
-        data.append(getNum(256));
-        //lowalpha
-        data.append(getNum(256));
-        data.append(getNum(256));
-        data.append(getNum(256));
-        //highalpha
-        data.append(getNum(256));
-        data.append(getNum(256));
-        data.append(getNum(256));
-        //lowbeta
-        data.append(getNum(256));
-        data.append(getNum(256));
-        data.append(getNum(256));
-        //highbeta
-        data.append(getNum(256));
-        data.append(getNum(256));
-        data.append(getNum(256));
-        //lowgamma
-        data.append(getNum(256));
-        data.append(getNum(256));
-        data.append(getNum(256));
-        //middlegamma
-        data.append(getNum(256));
-        data.append(getNum(256));
-        data.append(getNum(256));
-        //attention
-        data.append(0x04);
-        data.append(getNum(101));
-        //meditation
-        data.append(0x05);
-        data.append(getNum(101));
-
-        //calculate sumcheck
-        int checksum = data[3];
-        for (int i = 4; i <= data.size() - 1; i++)
-        {
-            checksum += data[i];
-        }
-        checksum &= 0xff;
-        checksum = ~checksum & 0xff;
-        data.append(checksum);
-        emit sendData(data);
+        QByteArray ba=getEEG();
+        emit sendData(ba);
         mutex.unlock();
     });
     timer->start();
--- src/generator.h
+++ src/generator.h
@@ -10,46 +10,6 @@
 #include <QVector>
 #include <QMutex>
 
-typedef struct type1
-{
-    int ASYN1;
-    int ASYN2;
-    int PAYLOAD;
-    int Signal;
-    int SignalValue;
-    int Power;
-    int PowerValue;
-    int Delta1;
-    int Delta2;
-    int Delta3;
-    int Theta1;
-    int Theta2;
-    int Theta3;
-    int LowAlpha1;
-    int LowAlpha2;
-    int LowAlpha3;
-    int HighAlpha1;
-    int HightAlpha2;
-    int HightAlpha3;
-    int LowBeta1;
-    int LowBeta2;
-    int LowBeta3;
-    int HighBeta1;
-    int HighBeta2;
-    int HighBeta3;
-    int LowGamma1;
-    int LowGamma2;
-    int LowGamma3;
-    int MiddleGamma1;
-    int MiddleGamma2;
-    int MiddleGamma3;
-    int Attention;
-    int AttentionValue;
-    int Meditation;
-    int MeditationValue;
-    int checksum;
-} type1;
-
 class Generator : public QThread
 {
     Q_OBJECT
@@ -58,7 +18,10 @@ public:
     ~Generator();
 
     int getNum(int max);//[0,max)
-    int getNum();
+    int getNum();//[0,256)
+
+    QByteArray getRaw();
+    QByteArray getEEG();
 
     void stopNow();
 
