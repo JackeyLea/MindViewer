cfd3898f51f649e7235fb21df6c7fcb6f488e8bb
Jackey Lea <1768478912@qq.com>
2024-09-28 19:08:33 +0800

    添加数据激励基类

---
+++ MindViewer/MindViewer/BaseStimulation.h
@@ -0,0 +1,24 @@
+/////////////////////////
+///
+/// \author Jackey Lea
+/// \date 2024-09-28
+/// \note 数据激励的基类
+///
+
+#ifndef BASESTIMULATION_H
+#define BASESTIMULATION_H
+
+#include <QObject>
+#include <QByteArray>
+
+class BaseStimulation : public QObject
+{
+public:
+    BaseStimulation();
+    ~BaseStimulation();
+
+protected:
+    void sigNewFrame(QByteArray ba);
+};
+
+#endif // BASESTIMULATION_H
--- MindViewer/MindViewer/MindViewer.pro
+++ MindViewer/MindViewer/MindViewer.pro
@@ -13,6 +13,7 @@ SOURCES += \
     mainwindow.cpp
 
 HEADERS += \
+    BaseStimulation.h \
     mainwindow.h
 
 FORMS += \
