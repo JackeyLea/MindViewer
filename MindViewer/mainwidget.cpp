fd7f98d8fb7c74a30ace60ed66a0b403d333c925
Jackey Lea <1768478912@qq.com>
2024-10-29 15:59:33 +0800

    添加界面按钮事件

--- MindViewer/mainwidget.cpp
+++ MindViewer/mainwidget.cpp
@@ -6,10 +6,58 @@ MainWidget::MainWidget(QWidget *parent)
     , ui(new Ui::MainWidget)
 {
     ui->setupUi(this);
+
+    initConn();
 }
 
 MainWidget::~MainWidget()
 {
     delete ui;
+}
+
+void MainWidget::initConn()
+{
+    connect(ui->btnCOM,&QPushButton::clicked,this,&MainWidget::sltBtnCOM);
+    connect(ui->btnSim,&QPushButton::clicked,this,&MainWidget::sltBtnSIM);
+    connect(ui->btnLocal,&QPushButton::clicked,this,&MainWidget::sltBtnLocal);
+    connect(ui->btnPlay,&QPushButton::clicked,this,&MainWidget::sltBtnPlay);
+    connect(ui->btnPause,&QPushButton::clicked,this,&MainWidget::sltBtnPause);
+    connect(ui->btnClear,&QPushButton::clicked,this,&MainWidget::sltBtnClear);
+    connect(ui->btnSave,&QPushButton::clicked,this,&MainWidget::sltBtnSave);
+}
+
+void MainWidget::sltBtnCOM()
+{
+
+}
+
+void MainWidget::sltBtnSIM()
+{
+
+}
+
+void MainWidget::sltBtnLocal()
+{
+
+}
+
+void MainWidget::sltBtnPlay()
+{
+
+}
+
+void MainWidget::sltBtnPause()
+{
+
+}
+
+void MainWidget::sltBtnClear()
+{
+
+}
+
+void MainWidget::sltBtnSave()
+{
+
 }
 
--- MindViewer/mainwidget.h
+++ MindViewer/mainwidget.h
@@ -15,6 +15,49 @@ public:
     MainWidget(QWidget *parent = nullptr);
     ~MainWidget();
 
+protected:
+    /// 初始化信号槽
+    /// \brief initConn
+    ///
+    void initConn();
+
+private slots:
+
+    /// 串口按钮响应
+    /// \brief sltBtnCOM
+    ///
+    void sltBtnCOM();
+
+    /// 仿真数据按钮响应
+    /// \brief sltBtnSIM
+    ///
+    void sltBtnSIM();
+
+    /// 打开本地文件按钮响应
+    /// \brief sltBtnLocal
+    ///
+    void sltBtnLocal();
+
+    /// 播放按钮响应
+    /// \brief sltBtnPlay
+    ///
+    void sltBtnPlay();
+
+    /// 暂停按钮响应
+    /// \brief sltBtnPause
+    ///
+    void sltBtnPause();
+
+    /// 清空按钮响应 清空界面数据和缓存区数据
+    /// \brief sltBtnClear
+    ///
+    void sltBtnClear();
+
+    /// 保存按钮响应 保存串口收到的数据
+    /// \brief sltBtnSave
+    ///
+    void sltBtnSave();
+
 private:
     Ui::MainWidget *ui;
 };
