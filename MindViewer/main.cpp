efd2d5978d7d9ac0747e472c341832fa4ffd9db9
Jackey Lea <1768478912@qq.com>
2024-10-29 16:33:42 +0800

    添加软件图标

--- MindViewer/main.cpp
+++ MindViewer/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWidget w;
+    w.setWindowTitle(QObject::tr("MindViewer"));
+    w.setWindowIcon(QIcon(":/resource/icon/mind.svg"));
     w.show();
     return a.exec();
 }
