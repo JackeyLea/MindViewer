53cfcc49aeb20f74f808034d76fefd888f28f9d1
Jackey Lea <1768478912@qq.com>
2024-10-31 17:49:45 +0800

    修复清空崩溃问题

--- MindViewer/curves.cpp
+++ MindViewer/curves.cpp
@@ -120,7 +120,7 @@ void Curves::updateData(_eegPkt pkt)
     int newSize = pkt.raw.size();
     //保持maxCnt个数据
     if(dataRaw.size()>=maxCnt){
-        dataRaw.remove(0,newSize);
+        dataRaw.remove(0,qMax(newSize,dataRaw.size()));
     }
     dataRaw.append(pkt.raw);
 
