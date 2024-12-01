6f0bd37e5ae2c65dfdd620cd3a9e94cef4311e5f
Jackey Lea <1768478912@qq.com>
2024-12-01 17:30:22 +0800

    添加桌面大小判断

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mainwidget.h"
 
 #include <QApplication>
+#include <QScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    //判断分辨率
+    QRect resolution = QApplication::primaryScreen()->geometry();
+
     MainWidget w;
+    if(resolution.width()<1080 || resolution.height()<720){
+        QMessageBox::warning(&w,QMessageBox::tr("警告"),QMessageBox::tr("屏幕过小，无法完全显示界面"));
+        return -1;
+    }
     w.setWindowTitle(QObject::tr("MindViewer"));
     w.setWindowIcon(QIcon(":/resource/icon/mind.svg"));
     w.setStyleSheet(getQSS());//加载样式表
